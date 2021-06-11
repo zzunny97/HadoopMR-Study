498921caa050c331ea4f46342036fc5c0d9c0c73
zzunny97 <zzunny97@gmail.com>
2021-06-11 15:12:33 +0900

    Done:)

--- app/src/main/java/demo/WordCount.java
+++ app/src/main/java/demo/WordCount.java
@@ -1,6 +1,10 @@
 package demo;
 
 import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -18,13 +22,35 @@ public class WordCount {
 
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
     }
 
     public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
     }
 
     public static void main(String[] args) throws Exception {
+
         Configuration conf = new Configuration();
         String[] pathArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (pathArgs.length < 2) {
